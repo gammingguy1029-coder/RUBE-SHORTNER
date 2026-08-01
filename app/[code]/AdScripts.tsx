e58cdfdc48da3f27c07a4273ea7261ce8adae14b
gammingguy1029-coder <gammingguy1029@gmail.com>
2026-08-01 23:57:30 +0530

    Update AdScripts.tsx

--- app/[code]/AdScripts.tsx
+++ app/[code]/AdScripts.tsx
@@ -1,17 +1,5 @@
 "use client";
-import { useEffect } from "react";
 
 export default function AdScripts() {
-  useEffect(() => {
-    const s2 = document.createElement("script");
-    s2.src = "https://pl30636549.effectivecpmnetwork.com/8f/c0/e0/8fc0e0bbeec98414982958c8a7105a2e.js";
-    s2.async = true;
-    document.body.appendChild(s2);
-
-    return () => {
-      s2.remove();
-    };
-  }, []);
-
   return null;
 }
